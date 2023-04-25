2542eeca280338144c5b09fdd5d2598392f44ed7
JuanD1206 <76623625+joker120620@users.noreply.github.com>
2023-04-25 15:17:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const fs = require('fs');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json());
 const port = 5000
-let codes=[]
-let session=false;
+let codes = []
+function crearCliente(name) {
+  const client = new Client({
+    authStrategy: new LocalAuth({ clientId: `${name}` })
+  });
+  return client
+}
+const client = crearCliente('base')
 console.log('iniciando bot')
-  client.on('qr', qr => {
-    codes[0]=`https://quickchart.io/qr?text=${qr}&size=1000&ecLevel=q&centerImageUrl=https://i.ibb.co/3BbYsWp/logo-2.jpg&format=png`
-    //comentario temporal
-    qrcode.generate(qr, {small: true});
-    
-  });
-  client.on('ready', () => {
-    console.log('Bot conectado');
-    client.sendMessage('573133085614@c.us' , 'servicio iniciado');
-    session=true;
-    codes[0]=''
-  });
-///fuctions del bot
-botSendMenssage(client, 'hola', 'holi')
-botSendImages(client,'Hola en que te puedo ayudar??', MessageMedia)
-  client.initialize();
+client.on('qr', qr => {
+  codes[0] = qr
+  //comentario temporal
+  qrcode.generate(qr, { small: true });
+
+});
+client.on('ready', () => {
+  console.log('Bot conectado');
+  client.sendMessage('573133085614@c.us', 'servicio iniciado');
+  session = true;
+  codes[0] = ''
+});
+client.initialize();
+client.on('message', async message => {
+  if (message.body=='hola') {
+      const media = await MessageMedia.fromUrl('https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067');
+      this.client.sendMessage(message.from, media, { caption: 'hi' });
+  }
+  
+});
 app.get('/', (req, res) => {
   res.send('server online')
 })
-app.post('/getcode', async (req, res) => {
-  if(req.body.token=='12345'){
-    if(session){
-      res.json({"qrcode":"none", "msj": "session ya iniciada"})
-    }else{
-      res.json({"qrcode":codes[0], "msj": "Escanea el Codigo"})
-    }
-    
-  }else{
-    res.json({"qrcode":"none" , "msj": "token Incorrecto"})
-  }
+app.get('/getcode', async (req, res) => {
+  res.json({ "qrcode": codes[0], "msj": "Escanea el Codigo" })
 })
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`)
